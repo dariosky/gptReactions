ef9e30fe69a396806037aa10a5eaefd87ddb72d7
Dario Varotto <dario.varotto@gsr.io>
2023-05-28 16:25:07 +0200

    /meteo text update

--- weather/weather_commands.py
+++ weather/weather_commands.py
@@ -5,10 +5,15 @@ def add_weather_commands(bot):
     @bot.command("/meteo")
     def repeat_text(ack, respond, command):
         # Acknowledge command request
-        request = command["text"]
+        request = command["text"].strip()
         ack()
+        if not request:
+            return respond(
+                "Please give me a request - ex. `/meteo tomorrow in New York`"
+            )
         respond(
-            f"Hi @{command['user_id']}. Let me check the weather...\n" f"> {request}"
+            f"Hello @{command['user_name']}. Let me check the weather...\n"
+            f"> {request}"
         )
         forecast = text_to_weather(request)
         respond(forecast)
